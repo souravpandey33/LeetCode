8194b42d7463b8db334c22ebb3bbc9d4ffc4139d
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-06-07 19:50:46 +0530

    Time: 418 ms (20.63%), Space: 144.8 MB (43.55%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -16,9 +16,8 @@ public:
             return 0;
         if(root->right==NULL)
             return 1+minDepth(root->left);
-        else if(root->left==NULL)
+        if(root->left==NULL)
             return 1+minDepth(root->right);
-        else
-            return 1+min(minDepth(root->right),minDepth(root->left));
+        return 1+min(minDepth(root->right),minDepth(root->left));
     }
 };
