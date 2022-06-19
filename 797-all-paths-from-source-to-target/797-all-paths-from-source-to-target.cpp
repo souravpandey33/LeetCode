0ce9c2339d66a9b95e9a090da856a168bf71025f
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-06-19 19:09:17 +0530

    Time: 11 ms (92.11%), Space: 10.7 MB (83.65%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> result;
+        vector<int> path;
+        int n=graph.size();
+        vector<bool> visit(n,false);
+        path.push_back(0);
+        check(graph,result,path,0,n,visit);
+        return result;
+    }
+    
+    void check(vector<vector<int>>& graph,vector<vector<int>> &result,vector<int> &path, int curr,int n, vector<bool> &visit)
+    {
+        if(curr==n-1)
+        {
+            result.push_back(path);
+            return;
+        }
+        
+        if(visit[curr]==true)
+            return;
+        
+        visit[curr]=true;
+        
+        for(auto neigh:graph[curr])
+        {
+            path.push_back(neigh);
+            check(graph,result,path,neigh,n,visit);
+            path.pop_back();
+        }
+        
+        visit[curr]=false;
+    }
+};
