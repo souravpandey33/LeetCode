be4df52083562240231e891d8ae0eeb2ca9a955c
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-06-21 08:57:16 +0530

    Time: 12 ms (6.93%), Space: 10.1 MB (44.53%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -15,9 +15,10 @@ public:
         if(p==NULL && q==NULL)
             return true;
         if(p==NULL || q==NULL)
+           return false;
+        if(p->val != q->val)
             return false;
-        if(p->val!=q->val)
-            return false;
+        
         return (isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
     }
 };
