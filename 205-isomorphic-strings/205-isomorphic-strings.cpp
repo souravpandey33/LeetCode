9d1727a695508f63386c10ff5ef43223a6e2ebb9
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-06-23 20:38:39 +0530

    Time: 9 ms (59.50%), Space: 6.9 MB (92.29%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size()!=t.size())
+            return false;
+        unordered_map<char,char> hmap;
+        vector<int> count(256,0);
+        for(int i=0;i<s.size();i++)
+        {
+            if(hmap.find(t[i])!=hmap.end())
+            {
+                if(hmap[t[i]]!=s[i])
+                    return false;
+            }
+            else
+            {
+                hmap[t[i]]=s[i];
+                count[s[i]]++;
+            }
+        }
+        
+        for(int i=0;i<256;i++)
+        {
+            if(count[i]>1)
+                return false;
+        }
+        return true;
+    }
+};
