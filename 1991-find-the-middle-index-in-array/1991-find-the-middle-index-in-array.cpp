0fafb3c3cd118c697e428c099dee3b78d907d033
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-06-23 08:00:14 +0530

    Time: 18 ms (5.46%), Space: 12.4 MB (30.89%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
@@ -1,58 +1,62 @@
-// class Solution {
-// public:
-//     int findMiddleIndex(vector<int>& nums) {
-//         int n=nums.size();
-//         int leftSum=0;
-//         int sum=0;
-//         for(int i=0;i<n;i++)
-//         {
-//             sum=sum+nums[i];
-//         }
-//         int rightSum=sum-nums[0];
-        
-        
-//         for(int i=0;i<n;i++)
-//         {
-//             if(leftSum==rightSum)
-//                 return i;
-//             if(i<n-1){
-//                 rightSum=rightSum-nums[i+1];
-//                 leftSum=leftSum+nums[i];
-//             }
-//         }
-//         return -1;
-//     }
-// };
-
-
-
+// Iterative Solution
 
 
 
 class Solution {
 public:
-    int index=-1;
     int findMiddleIndex(vector<int>& nums) {
-        
         int n=nums.size();
+        int leftSum=0;
         int sum=0;
         for(int i=0;i<n;i++)
         {
             sum=sum+nums[i];
         }
-        check(nums,n,0,sum-nums[0],0);
-        return index;
-    }
-    
-    void check(vector<int> &nums, int n, int curr, int sumRight, int sumLeft)
-    {
-        if(sumRight==sumLeft)
+        int rightSum=sum-nums[0];
+        
+        
+        for(int i=0;i<n;i++)
         {
-            index=curr;
-            return;
+            if(leftSum==rightSum)
+                return i;
+            if(i<n-1){
+                rightSum=rightSum-nums[i+1];
+                leftSum=leftSum+nums[i];
+            }
         }
-        if(curr==n-1)
-            return;
-        check(nums,n,curr+1,sumRight-nums[curr+1], sumLeft+nums[curr]);
+        return -1;
     }
 };
+
+
+
+// Recursive Solution
+
+
+// class Solution {
+// public:
+//     int index=-1;
+//     int findMiddleIndex(vector<int>& nums) {
+        
+//         int n=nums.size();
+//         int sum=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             sum=sum+nums[i];
+//         }
+//         check(nums,n,0,sum-nums[0],0);
+//         return index;
+//     }
+    
+//     void check(vector<int> &nums, int n, int curr, int sumRight, int sumLeft)
+//     {
+//         if(sumRight==sumLeft)
+//         {
+//             index=curr;
+//             return;
+//         }
+//         if(curr==n-1)
+//             return;
+//         check(nums,n,curr+1,sumRight-nums[curr+1], sumLeft+nums[curr]);
+//     }
+// };
