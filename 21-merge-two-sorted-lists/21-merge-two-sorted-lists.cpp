a30d4f2d0050eb8c999815210bf9d435061e2fe1
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-06-24 11:43:00 +0530

    Time: 19 ms (14.48%), Space: 14.8 MB (42.59%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,33 +11,33 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* temp=new ListNode(0);
-        ListNode* ptr=temp;
+        ListNode* merge=new ListNode(0);
+        ListNode* ptr=merge;
         while(list1 && list2)
         {
             if(list1->val <= list2->val)
             {
-                temp->next=list1;
+                merge->next=list1;
                 list1=list1->next;
             }
             else
             {
-                temp->next=list2;
+                merge->next=list2;
                 list2=list2->next;
             }
-            temp=temp->next;
+            merge=merge->next;
         }
         while(list1)
         {
-            temp->next=list1;
+            merge->next=list1;
+            merge=merge->next;
             list1=list1->next;
-            temp=temp->next;
         }
         while(list2)
         {
-            temp->next=list2;
+            merge->next=list2;
+            merge=merge->next;
             list2=list2->next;
-            temp=temp->next;
         }
         return ptr->next;
     }
