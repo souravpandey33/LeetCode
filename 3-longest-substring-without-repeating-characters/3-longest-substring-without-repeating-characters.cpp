d04e0c8266a22f945ec823986601381ac603d144
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-06-26 21:55:02 +0530

    Time: 15 ms (73.85%), Space: 8.4 MB (46.72%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -3,10 +3,8 @@ public:
     int lengthOfLongestSubstring(string s) {
         unordered_map<char,int> mp;
         int size=s.size();
-        int i=0;
         int j=0;
         int start=0;
-        int end=0;
         int mx=0;
         
         if(size==1)
@@ -16,18 +14,14 @@ public:
             if(mp.find(s[j])!=mp.end())
             {
                 start=max(start,mp[s[j]]+1);
-                cout<<start<<endl;
                 mp[s[j]]=j;
             }
             else
             {
                 mp[s[j]]=j;
             }
-            
             j++;
-            end++;
-            cout<<start<<"  "<<end<<endl;
-            mx=max(mx,end-start);
+            mx=max(mx,j-start);
         }
         return mx;
     }
