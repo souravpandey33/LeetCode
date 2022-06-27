0353c0c2e1d33a667906d6f784f8f700c74aec5a
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-06-27 19:31:05 +0530

    Time: 354 ms (10.07%), Space: 73.2 MB (23.71%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -5,25 +5,30 @@ public:
         int j=0;
         int i=0;
         int ans=0;
+        int count=0;
         while(j<s.size())
         {
             mp[s[j]]++;
-            if(mp.size()==2)
+            if(mp[s[j]]==1)
+            {
+                count++;
+            }
+            if(count==2)
             {
                 ans=max(ans,j-i+1);
             }
-            else if(mp.size()<2)
+            else if(count<2)
             {
                 ans=j-i+1;
             }
             else
             {
-                while(mp.size()>2 && i<=j)
+                while(count>2 && i<=j)
                 {
                     mp[s[i]]--;
                     if(mp[s[i]]==0)
                     {
-                        mp.erase(s[i]);
+                        count--;
                     }
                     i++;
                 }
