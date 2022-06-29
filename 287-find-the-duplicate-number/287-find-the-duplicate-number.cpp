f9e1732de8c29a669dbc9634415ee80f4f844816
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-06-29 10:46:27 +0530

    Time: 222 ms (30.55%), Space: 61.3 MB (27.99%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = 0;
-        int fast = 0;
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        slow = 0;
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
+        int rep=0;
         
-        return slow;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[abs(nums[i])-1]<0)
+            {
+                rep=abs(nums[i]);
+                break;
+            }
+            else
+            {
+                nums[abs(nums[i])-1]*=-1;
+            }
+        }
+        return rep;
     }
 };
