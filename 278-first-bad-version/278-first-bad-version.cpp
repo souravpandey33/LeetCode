e41ffed043b040320c825741b4636717481cf0fb
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-07-01 10:41:05 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (67.43%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -1,24 +1,47 @@
-// The API isBadVersion is defined for you.
-// bool isBadVersion(int version);
+// // The API isBadVersion is defined for you.
+// // bool isBadVersion(int version);
 
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int end=n;
         int start=1;
-        int mid;
+        int end=n;
         int ans=1;
+        int mid;
+        
         while(start<=end)
         {
             mid=start+(end-start)/2;
-            if(isBadVersion(mid)) // bad from mid or before mid
+            if(isBadVersion(mid))
             {
                 ans=mid;
                 end=mid-1;
             }
             else
-                start=mid+1; // bad after mid
+                start=mid+1;
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int firstBadVersion(int n) {
+//         int end=n;
+//         int start=1;
+//         int mid;
+//         int ans=1;
+//         while(start<=end)
+//         {
+//             mid=start+(end-start)/2;
+//             if(isBadVersion(mid)) // bad from mid or before mid
+//             {
+//                 ans=mid;
+//                 end=mid-1;
+//             }
+//             else
+//                 start=mid+1; // bad after mid
+//         }
+//         return ans;
+//     }
+// };
