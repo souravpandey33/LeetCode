f14e45e9097444cedfd91b19e6813c82ef7fa363
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-07-01 19:14:59 +0530

    Time: 51 ms (54.04%), Space: 12.3 MB (58.07%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -4,29 +4,30 @@ public:
         int row=grid.size();
         int col=grid[0].size();
         int ans=0;
-        for(int currRow=0;currRow<row;currRow++)
+        for(int i=0;i<row;i++)
         {
-            for(int currCol=0;currCol<col;currCol++)
+            for(int j=0;j<col;j++)
             {
-                if(grid[currRow][currCol]=='1')
+                if(grid[i][j]=='1')
                 {
                     ans++;
-                    check(grid,row,col,currRow,currCol);
+                    check(grid,i,j,row,col);
                 }
             }
         }
         return ans;
     }
     
-    void check(vector<vector<char>> &grid, int row, int col, int currRow, int currCol)
+    void check(vector<vector<char>> &grid,int row, int col, int maxrow, int maxcol)
     {
-        if(currRow>=row || currRow<0 || currCol>=col || currCol<0 || grid[currRow][currCol]=='0')
+        if(row>=maxrow || col>=maxcol || col<0 || row<0 || grid[row][col]=='0')
             return;
         
-        grid[currRow][currCol]='0';
-        check(grid,row,col,currRow+1,currCol);
-        check(grid,row,col,currRow-1,currCol);
-        check(grid,row,col,currRow,currCol+1);
-        check(grid,row,col,currRow,currCol-1);
+        grid[row][col]='0';
+        check(grid,row+1,col,maxrow,maxcol);
+        check(grid,row-1,col,maxrow,maxcol);
+        check(grid,row,col+1,maxrow,maxcol);
+        check(grid,row,col-1,maxrow,maxcol);
     }
 };
+
