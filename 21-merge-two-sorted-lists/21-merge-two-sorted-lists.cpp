38fca47cfad2d1e7f5447aadfa18f9c785df48f8
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-07-02 10:53:24 +0530

    Time: 8 ms (79.04%), Space: 14.7 MB (81.16%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,35 +10,58 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* merge=new ListNode(0);
-        ListNode* ptr=merge;
-        while(list1 && list2)
+    // ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    //     ListNode* merge=new ListNode(0);
+    //     ListNode* ptr=merge;
+    //     while(list1 && list2)
+    //     {
+    //         if(list1->val <= list2->val)
+    //         {
+    //             merge->next=list1;
+    //             list1=list1->next;
+    //         }
+    //         else
+    //         {
+    //             merge->next=list2;
+    //             list2=list2->next;
+    //         }
+    //         merge=merge->next;
+    //     }
+    //     while(list1)
+    //     {
+    //         merge->next=list1;
+    //         merge=merge->next;
+    //         list1=list1->next;
+    //     }
+    //     while(list2)
+    //     {
+    //         merge->next=list2;
+    //         merge=merge->next;
+    //         list2=list2->next;
+    //     }
+    //     return ptr->next;
+    // }
+    
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
+    {
+        if(list1==NULL)
         {
-            if(list1->val <= list2->val)
-            {
-                merge->next=list1;
-                list1=list1->next;
-            }
-            else
-            {
-                merge->next=list2;
-                list2=list2->next;
-            }
-            merge=merge->next;
+            return list2;
         }
-        while(list1)
+        if(list2==NULL)
         {
-            merge->next=list1;
-            merge=merge->next;
-            list1=list1->next;
+            return list1;
         }
-        while(list2)
+        
+        if(list1->val < list2->val)
         {
-            merge->next=list2;
-            merge=merge->next;
-            list2=list2->next;
+            list1->next=mergeTwoLists(list1->next,list2);
+            return list1;
         }
-        return ptr->next;
+        else
+        {
+            list2->next=mergeTwoLists(list1,list2->next);
+            return list2;
+        }
     }
 };
