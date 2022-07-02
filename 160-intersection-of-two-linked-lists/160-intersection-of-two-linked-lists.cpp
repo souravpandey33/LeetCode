78ba73f9277fa1d831696062955ebd469b0e0626
Sourav Pandey <60563930+souravpandey33@users.noreply.github.com>
2022-07-02 12:29:10 +0530

    Time: 81 ms (34.76%), Space: 14.4 MB (98.87%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,53 +9,71 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* tempA=headA;
-        ListNode* tempB=headB;
+//         ListNode* tempA=headA;
+//         ListNode* tempB=headB;
         
-        int ca=0;
-        int cb=0;
+//         int ca=0;
+//         int cb=0;
         
-        while(tempA)
-        {
-            ca++;
-            tempA=tempA->next;
+//         while(tempA)
+//         {
+//             ca++;
+//             tempA=tempA->next;
+//         }
+        
+//         while(tempB)
+//         {
+//             cb++;
+//             tempB=tempB->next;
+//         }
+        
+//         int diff=0;
+        
+//         if(ca>cb)
+//         {
+//             diff=ca-cb;
+//             while(diff--)
+//             {
+//                 headA=headA->next;
+//             }
+//         }
+//         else
+//         {
+//             diff=cb-ca;
+//             while(diff--)
+//             {
+//                 headB=headB->next;
+//             }
+//         }
+        
+//         while(headA && headB)
+//         {
+//             if(headA==headB)
+//             {
+//                 return headA;
+//             }
+//             headA=headA->next;
+//             headB=headB->next;
+//         }
+        
+//         return NULL;
+        
+        ListNode* A=headA;
+        ListNode* B=headB;
+    
+        while(A!=NULL || B!=NULL){
+        
+            if(A==B) return A;
+        
+            A=A->next;
+            B=B->next;
+        
+            if(A==NULL && B!=NULL)
+                A=headB;
+            if(B==NULL && A!=NULL)
+                B=headA;
+    
         }
-        
-        while(tempB)
-        {
-            cb++;
-            tempB=tempB->next;
-        }
-        
-        int diff=0;
-        
-        if(ca>cb)
-        {
-            diff=ca-cb;
-            while(diff--)
-            {
-                headA=headA->next;
-            }
-        }
-        else
-        {
-            diff=cb-ca;
-            while(diff--)
-            {
-                headB=headB->next;
-            }
-        }
-        
-        while(headA && headB)
-        {
-            if(headA==headB)
-            {
-                return headA;
-            }
-            headA=headA->next;
-            headB=headB->next;
-        }
-        
         return NULL;
     }
 };
